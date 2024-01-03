be3b3224d8c45bb599a058fa28ddbfe177abc2a2
RobertoDPH <robertodelpino11@gmail.com>
2024-01-03 18:18:36 +0000

    adding comments

--- src/Users/Infraestructure/Repositories/userRepository.ts
+++ src/Users/Infraestructure/Repositories/userRepository.ts
@@ -2,6 +2,11 @@ import { User } from '../../Domain/entities/User';
 import { IUserRepository } from '../../Domain/interfaces/userRepository';
 
 
+// This is the implementation of the repository
+// This way is wrong, because we are using an array to store the users
+// We should use a database to store the users
+// But just for the example, we are using an array
+// Next task to do will be to use a database
 export class userRepository implements IUserRepository {
     private users: User[];
 
