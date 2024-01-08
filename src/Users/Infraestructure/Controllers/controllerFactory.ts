689480b5d3fd210bcff23bfe8d391d4937373ad3
RobertoDPH <robertodelpino11@gmail.com>
2024-01-08 10:43:50 +0000

    add route and factory method

--- src/Users/Infraestructure/Controllers/controllerFactory.ts
+++ src/Users/Infraestructure/Controllers/controllerFactory.ts
@@ -3,6 +3,8 @@ import { CreateUserController } from "./createUserController";
 import { createToken } from "../../Domain/services/createToken";
 import { sendEmail } from "../Services/sendEmail";
 import { mongoDbUserRepository } from "../Repositories/MongoDB/userRepository";
+import { ConfirmUserUseCase } from "../../Application/confirmUserUseCase";
+import { ConfirmUserController } from "./confirmUserController";
 
 
 function createCreateUserController() {
@@ -13,4 +15,10 @@ function createCreateUserController() {
   return new CreateUserController(useCase);
 }
 
-export { createCreateUserController };
+function createConfirmUserController(){
+  const repository = new mongoDbUserRepository();
+  const useCase = new ConfirmUserUseCase(repository);
+  return new ConfirmUserController(useCase);
+}
+
+export { createCreateUserController, createConfirmUserController };
--- src/Users/Infraestructure/routes.ts
+++ src/Users/Infraestructure/routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { createCreateUserController } from './Controllers/controllerFactory';
+import { createConfirmUserController, createCreateUserController } from './Controllers/controllerFactory';
 
 const router = express.Router();
 
 const createUserController = createCreateUserController();
+const confirmUserController = createConfirmUserController();
 
 router.post('/users/create', createUserController.handle.bind(createUserController));
+router.post('/users/confirm/:token', confirmUserController.handle.bind(confirmUserController));
 
 /*router.get('/users/:id', userController.getUserById);
 router.post('/users', userController.createUser);
