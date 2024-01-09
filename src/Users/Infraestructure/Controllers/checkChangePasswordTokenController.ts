3355b0cc0d9097fb318003ef15eadd07a2b2de7e
RobertoDPH <robertodelpino11@gmail.com>
2024-01-09 16:03:02 +0000

    created controller

---
+++ src/Users/Infraestructure/Controllers/checkChangePasswordTokenController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { ICheckChangePasswordTokenUseCase } from '../../Application/checkChangePasswordTokenUseCase';
+
+export class checkChangePasswordTokenController {
+    private useCase: ICheckChangePasswordTokenUseCase;
+
+    constructor(useCase: ICheckChangePasswordTokenUseCase) {
+        this.useCase = useCase;
+    }
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        try {
+            const { token } = request.params;
+
+            await this.useCase.execute(token);
+
+            return response.status(200).json({ message: 'Token is valid' });
+        } catch (error: any) {
+            return response.status(400).json({ error: error.message });
+        }
+    }
+}
+
+
