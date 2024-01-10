5dcaba3ccad81cf93ee139f06e7ebf0c0d7a2c46
RobertoDPH <robertodelpino11@gmail.com>
2024-01-10 18:39:24 +0000

    Added route

--- src/Users/Infraestructure/routes.ts
+++ src/Users/Infraestructure/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { 
+    createChangePasswordController,
     createCheckChangePasswordTokenController,
     createConfirmUserController, 
     createCreateUserController,
@@ -12,10 +13,12 @@ const createUserController = createCreateUserController();
 const confirmUserController = createConfirmUserController();
 const forgotPasswordController = createForgotPasswordController();
 const checkChangePasswordTokenController = createCheckChangePasswordTokenController();
+const changePasswordController = createChangePasswordController();
 
 router.post('/users/create', createUserController.handle.bind(createUserController));
 router.post('/users/confirm/:token', confirmUserController.handle.bind(confirmUserController));
 router.post('/users/forgot-password', forgotPasswordController.handle.bind(forgotPasswordController));
 router.post('/users/check-change-password-token/:token', checkChangePasswordTokenController.handle.bind(checkChangePasswordTokenController));
+router.post('/users/change-password/:token', changePasswordController.handle.bind(changePasswordController));
 
 export default router;
