2a9d1a829b991afed8903f1754808c1d482c4c64
RobertoDPH <robertodelpino11@gmail.com>
2024-01-11 19:50:01 +0000

    prepare project to contain tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-    setupFilesAfterEnv: ['./tests/setup-test.ts'],
+    //setupFilesAfterEnv: ['./tests/setup-test.ts'],
+    testMatch: [
+        "**/tests/**/*.[jt]s?(x)", 
+        "**/?(*.)+(spec|test).[tj]s?(x)"
+    ],
 };
 
 export default config;
