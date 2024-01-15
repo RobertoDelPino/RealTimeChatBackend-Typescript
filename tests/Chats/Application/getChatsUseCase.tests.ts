c0452e8120c958fea571363eeb11d50993844ef5
RobertoDPH <robertodelpino11@gmail.com>
2024-01-15 17:00:03 +0000

    thinking how to implement the useCase

--- tests/Chats/Application/getChatsUseCase.tests.ts
+++ tests/Chats/Application/getChatsUseCase.tests.ts
@@ -18,3 +18,27 @@ describe("get chats use Case", () => {
         expect(chatsRepository.findAll).toReturnWith(chats);
     });
 })
+
+export interface IChatsRepository {
+    findAll(userId: string): Promise<Chat[]>;
+}
+
+export interface IGetChatsUseCase {
+    execute(userId: string): Promise<Chat[]>;
+}
+
+export class Chat {
+    constructor(
+        public _id: string,
+        public users: User[],
+        public messages: Message[]
+    ) {}
+}
+
+export class User {
+    constructor(
+        public _id?: string,
+        public name?: string,
+        public email?: string
+    ) {}
+}
