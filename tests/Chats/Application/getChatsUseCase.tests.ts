bafd68d2ff49e6e8df5eb9d450cd758e135ffaf3
RobertoDPH <robertodelpino11@gmail.com>
2024-01-15 17:04:33 +0000

    thinking how to implement the useCase

--- tests/Chats/Application/getChatsUseCase.tests.ts
+++ tests/Chats/Application/getChatsUseCase.tests.ts
@@ -42,3 +42,12 @@ export class User {
         public email?: string
     ) {}
 }
+
+export class Message {
+    constructor(
+        public _id?: string,
+        public content?: string,
+        public sender?: User,
+        public receiver?: User
+    ) {}
+}
