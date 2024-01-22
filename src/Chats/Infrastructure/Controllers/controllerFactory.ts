ac45df364702b7304268c5ae5ea07cced5247519
RobertoDPH <robertodelpino11@gmail.com>
2024-01-22 18:23:16 +0000

    Added factory and routes

--- src/Chats/Infrastructure/Controllers/controllerFactory.ts
+++ src/Chats/Infrastructure/Controllers/controllerFactory.ts
@@ -1,6 +1,8 @@
 import { GetChatUseCase } from "../../Application/getChatUseCase";
+import { GetChatsUseCase } from "../../Application/getChatsUseCase";
 import { mongoDbChatRepository } from "../Repositories/MongoDB/chatRepository";
 import { GetChatController } from "./getChatController";
+import { GetChatsController } from "./getChatsController";
 
 function createGetChatController() {
     const repository = new mongoDbChatRepository();
@@ -8,4 +10,10 @@ function createGetChatController() {
     return new GetChatController(useCase);
 }
 
-export { createGetChatController };
+function createGetChatsController() {
+    const repository = new mongoDbChatRepository();
+    const useCase = new GetChatsUseCase(repository);
+    return new GetChatsController(useCase);
+}
+
+export { createGetChatController, createGetChatsController };
--- src/Chats/Infrastructure/routes.ts
+++ src/Chats/Infrastructure/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { createGetChatController } from './Controllers/controllerFactory';
+import { createGetChatController, createGetChatsController } from './Controllers/controllerFactory';
 
 const router = express.Router();
 
 const getChatController = createGetChatController();
+const getChatsController = createGetChatsController();
 
 router.get('/chats/:chatId', getChatController.execute.bind(getChatController));
+router.get('/user/:userId/chats', getChatsController.execute.bind(getChatsController));
 
 export default router;
