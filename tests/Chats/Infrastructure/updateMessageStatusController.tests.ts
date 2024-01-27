e90d5ada4c25562aca96fd2a1956cd95419d7642
RobertoDPH <robertodelpino11@gmail.com>
2024-01-27 13:08:28 +0000

    green updates message status

---
+++ tests/Chats/Infrastructure/updateMessageStatusController.tests.ts
@@ -0,0 +1,46 @@
+import { getMockReq, getMockRes } from "@jest-mock/express";
+import { IUpdateMessageStatusUseCase } from "../../../src/Chats/Application/updateMessageStatusUseCase";
+import { updateMessageStatusUseCaseMock } from "../Application/mock/updateMessageStatusUseCaseMock";
+import { Request, Response } from "express";
+
+describe("updateMessageStatus Controller", () => {
+
+    let updateMessageStatusUseCase : IUpdateMessageStatusUseCase;
+    let updateMessageStatusController : UpdateMessageStatusController;
+
+    beforeEach(() => {
+        updateMessageStatusUseCase = updateMessageStatusUseCaseMock;
+        updateMessageStatusController = new UpdateMessageStatusController(updateMessageStatusUseCase);
+    });
+
+    it("updates message status", async () => {
+        const messageId = "12345678";
+        const chatId = "123";
+        const req = getMockReq({body: { chatId: chatId, messageId: messageId }});
+        const { res } = getMockRes();
+        updateMessageStatusUseCase.execute = jest.fn().mockReturnValue("Messages status updated");
+
+        await updateMessageStatusController.execute(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith("Messages status updated");
+    });
+});
+
+export interface IUpdateMessageStatusController {
+    execute(req: Request, res: Response): Promise<void>;
+}
+
+export class UpdateMessageStatusController implements IUpdateMessageStatusController {
+    constructor(private updateMessageStatusUseCase: IUpdateMessageStatusUseCase) {}
+
+    async execute(req: Request, res: Response): Promise<void> {
+        const { chatId, messageId } = req.body;
+        try {
+            const result = await this.updateMessageStatusUseCase.execute(chatId, messageId);
+            res.status(200).json(result);
+        } catch (error) {
+            res.status(400).json({error: error.message});
+        }
+    }
+}
