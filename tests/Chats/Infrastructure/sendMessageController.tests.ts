b802deb7349e313b4267b3a8e8c8e8d46bdf49ae
RobertoDPH <robertodelpino11@gmail.com>
2024-01-27 17:08:38 +0000

    green throws an error if Message is empty, green throws an error if SenderId is empty, green throws an error if chatId is empty

--- tests/Chats/Infrastructure/sendMessageController.tests.ts
+++ tests/Chats/Infrastructure/sendMessageController.tests.ts
@@ -52,6 +52,57 @@ describe('sendMessageController', () => {
         expect(res.status).toHaveBeenCalledWith(400);
         expect(res.json).toHaveBeenCalledWith({error: "Chat does not exists"});
     });
+
+    it("throws an error if chatId is empty", async () => {
+        const newMessage = new Message(
+            "messageId",
+            "content of the message",
+            new User("userId", "userName", "userEmail"),
+            new Date(),
+            false
+        );
+        const req = getMockReq({body: { chatId: "", sender: newMessage.sender, message: newMessage.content }});
+        const { res } = getMockRes();
+
+        await sendMessageController.execute(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({error: "ChatId is required"});
+    });
+
+    it("throws an error if senderId is empty", async () => {
+        const newMessage = new Message(
+            "messageId",
+            "content of the message",
+            new User("userId", "userName", "userEmail"),
+            new Date(),
+            false
+        );
+        const req = getMockReq({body: { chatId: newMessage._id, sender: "", message: newMessage.content }});
+        const { res } = getMockRes();
+
+        await sendMessageController.execute(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({error: "SenderId is required"});
+    });
+
+    it("throws an error if Message is empty", async () => {
+        const newMessage = new Message(
+            "messageId",
+            "content of the message",
+            new User("userId", "userName", "userEmail"),
+            new Date(),
+            false
+        );
+        const req = getMockReq({body: { chatId: newMessage._id, sender: newMessage.sender, message: "" }});
+        const { res } = getMockRes();
+
+        await sendMessageController.execute(req, res);
+
+        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.json).toHaveBeenCalledWith({error: "Message is required"});
+    });
 });
 
 export interface ISendMessageController {
@@ -65,6 +116,18 @@ export class SendMessageController implements ISendMessageController {
     async execute(req: Request, res: Response): Promise<void> {
         try{
             const { chatId, message, sender } = req.body;
+
+            if(!chatId){
+                throw new Error("ChatId is required");
+            }
+
+            if(!message){
+                throw new Error("Message is required");
+            }
+
+            if(!sender){
+                throw new Error("SenderId is required");
+            }
 
             const newMessage = new Message(
                 "",
