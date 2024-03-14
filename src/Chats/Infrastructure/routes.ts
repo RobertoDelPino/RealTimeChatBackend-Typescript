723aa03354d23ff441c4c841f7dc1c744380ae03
RobertoDPH <robertodelpino11@gmail.com>
2024-03-14 10:30:06 +0000

    improve routes names

--- src/Chats/Infrastructure/routes.ts
+++ src/Chats/Infrastructure/routes.ts
@@ -12,10 +12,10 @@ const createChatController = createCreateChatController();
 const updateMessageStatusController = createUpdateMessageStatusController();
 const sendMessageController = createSendMessageController();
 
-router.get('/chats/:chatId', checkAuth.checkAuth.bind(checkAuth) , getChatController.execute.bind(getChatController));
-router.get('/user/:userId/chats',  checkAuth.checkAuth.bind(checkAuth), getChatsController.execute.bind(getChatsController));
-router.post('/chats', checkAuth.checkAuth.bind(checkAuth), createChatController.execute.bind(createChatController));
-router.post('/chat/update-status', checkAuth.checkAuth.bind(checkAuth), updateMessageStatusController.execute.bind(updateMessageStatusController));
-router.post('/chat/send-message', checkAuth.checkAuth.bind(checkAuth), sendMessageController.execute.bind(sendMessageController));
+router.get('/api/chats/:chatId', checkAuth.checkAuth.bind(checkAuth) , getChatController.execute.bind(getChatController));
+router.get('/api/chats',  checkAuth.checkAuth.bind(checkAuth), getChatsController.execute.bind(getChatsController));
+router.post('/api/chats', checkAuth.checkAuth.bind(checkAuth), createChatController.execute.bind(createChatController));
+router.post('/api/chat/update-status', checkAuth.checkAuth.bind(checkAuth), updateMessageStatusController.execute.bind(updateMessageStatusController));
+router.post('/api/chat/send-message', checkAuth.checkAuth.bind(checkAuth), sendMessageController.execute.bind(sendMessageController));
 
 export default router;
--- src/Users/Infrastructure/routes.ts
+++ src/Users/Infrastructure/routes.ts
@@ -23,12 +23,12 @@ const loginController = createLoginController();
 // middleware
 const checkAuth = createCheckAuth();
 
-router.post('/users/create', createUserController.handle.bind(createUserController));
-router.post('/users/confirm/:token', confirmUserController.handle.bind(confirmUserController));
-router.post('/users/forgot-password', forgotPasswordController.handle.bind(forgotPasswordController));
-router.post('/users/check-change-password-token/:token', checkChangePasswordTokenController.handle.bind(checkChangePasswordTokenController));
-router.post('/users/change-password/:token', changePasswordController.handle.bind(changePasswordController));
-router.get("/users/profile", checkAuth.checkAuth.bind(checkAuth), getProfileController.execute.bind(getProfileController));
-router.post("/users/login", loginController.execute.bind(loginController));
+router.post('/api/users/create', createUserController.handle.bind(createUserController));
+router.post('/api/users/confirm/:token', confirmUserController.handle.bind(confirmUserController));
+router.post('/api/users/forgot-password', forgotPasswordController.handle.bind(forgotPasswordController));
+router.post('/api/users/check-change-password-token/:token', checkChangePasswordTokenController.handle.bind(checkChangePasswordTokenController));
+router.post('/api/users/change-password/:token', changePasswordController.handle.bind(changePasswordController));
+router.get("/api/users/profile", checkAuth.checkAuth.bind(checkAuth), getProfileController.execute.bind(getProfileController));
+router.post("/api/users/login", loginController.execute.bind(loginController));
 
 export default router;  
