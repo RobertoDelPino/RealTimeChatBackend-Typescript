62c690c14aa91287479641b0bcba512ac286ebda
RobertoDPH <robertodelpino11@gmail.com>
2024-03-14 10:44:52 +0000

    refactor

--- src/Chats/Infrastructure/Controllers/updateMessageStatusController.ts
+++ src/Chats/Infrastructure/Controllers/updateMessageStatusController.ts
@@ -9,12 +9,13 @@ export class UpdateMessageStatusController implements IUpdateMessageStatusContro
     constructor(private updateMessageStatusUseCase: IUpdateMessageStatusUseCase) {}
 
     async execute(req: Request, res: Response): Promise<void> {
-        const { chatId, userId } = req.body;
-        if(!chatId) res.status(400).json({error: "ChatId is required"});
-        if(!userId) res.status(400).json({error: "UserId is required"});
-
         try {
+            const { chatId, userId } = req.body;
+            if(!chatId) throw new Error("ChatId is required");
+            if(!userId) throw new Error("UserId is required");
+
             const result = await this.updateMessageStatusUseCase.execute(chatId, userId);
+            
             res.status(200).json(result);
         } catch (error) {
             res.status(400).json({error: error.message});
