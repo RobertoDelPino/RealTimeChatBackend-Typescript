b9ac9e85d20c0f4967789d7527ea573e5fa68642
RobertoDPH <robertodelpino11@gmail.com>
2024-03-19 18:06:24 +0000

    rename

--- src/Chats/Domain/Entities/Message.ts
+++ src/Chats/Domain/Entities/Message.ts
@@ -3,7 +3,7 @@ import { User } from "./User";
 export class Message {
     constructor(
         public _id?: string,
-        public content?: string,
+        public message?: string,
         public sender?: User,
         public createdAt?: Date,
         public readed: boolean = false,
