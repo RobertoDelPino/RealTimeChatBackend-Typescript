f2b21186f86818924deff1c7cc64960845f2b744
RobertoDPH <robertodelpino11@gmail.com>
2024-03-20 07:48:15 +0000

    Returns status code 400 when usecase fail

--- tests/Users/Infraestructure/updateUserProfileController.tests.ts
+++ tests/Users/Infraestructure/updateUserProfileController.tests.ts
@@ -13,12 +13,24 @@ describe("Update User Profile Controller", () => {
         updateUserProfileController = new UpdateUserProfileController(updateUserProfileUseCase);
     });
 
-    it("Should return status code 200", async () => {
+    it("Returns status code 200", async () => {
         const request = getMockReq({ body: { name: "name", password: "password" }, file: { path: "path" }});
         const response = getMockRes().res;
 
         await updateUserProfileController.execute(request, response);
 
         expect(response.status).toHaveBeenCalledWith(200);
+    });
+
+    it("Returns status code 400 when usecase fail", async () => {
+        const request = getMockReq({ body: { name: "name", password: "password" }, file: { path: "path" }});
+        const response = getMockRes().res;
+        updateUserProfileUseCase.execute = jest.fn().mockRejectedValue(new Error("Error saving user profile"))
+   
+
+        await updateUserProfileController.execute(request, response);
+
+        expect(response.status).toHaveBeenCalledWith(400);
+        expect(response.json).toHaveBeenCalledWith({ error: "Error saving user profile" });
     });
 });
