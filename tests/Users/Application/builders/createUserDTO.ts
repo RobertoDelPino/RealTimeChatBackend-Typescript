68c3663e3b31a99ff04479429f0af1e814c1a3a6
RobertoDPH <robertodelpino11@gmail.com>
2024-03-29 09:38:18 +0000

    ignore

--- tests/Users/Application/builders/createUserDTO.ts
+++ tests/Users/Application/builders/createUserDTO.ts
@@ -1,6 +1,3 @@
-// crea una funcion que recibe un objeto con los datos del usuario y retorna un objeto con los datos del usuario
-// Puede que el objeto que recibe no tenga datos y tenga que rellenarlos con valores por defecto
-
 import { UserDTO } from "../../../../src/Users/Application/searchUserByEmailUseCase";
 
 interface UserRequestDTO {
