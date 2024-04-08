e2be2a53496d74f100368d1e27a3581fef396699
RobertoDPH <robertodelpino11@gmail.com>
2024-04-08 09:46:19 +0100

    improve controller

--- src/Chats/Infrastructure/Controllers/createChatController.ts
+++ src/Chats/Infrastructure/Controllers/createChatController.ts
@@ -9,10 +9,15 @@ export class CreateChatController implements ICreateChatController {
     constructor(private createChatUseCase: ICreateChatUseCase) {}
 
     async execute(req: Request, res: Response): Promise<void> {
-        const { users, chatName = "" } : ICreateChatRequest = req.body;
-        const chat = await this.createChatUseCase.execute(users, chatName);
-
-        res.status(200).json(chat);
+        try{
+            const { users, chatName = "" } : ICreateChatRequest = req.body;
+            const chat = await this.createChatUseCase.execute(users, chatName);
+    
+            res.status(200).json(chat);
+        }
+        catch(error) {
+            res.status(400).json({error: error.message});
+        }
     }
 }
 
